9556cbeedd67b750fda19320a752e79ba8abde81
Will Crichton <wcrichto@cs.stanford.edu>
2021-02-20 23:50:27 -0500

    CI

--- scripts/build_all.py
+++ scripts/build_all.py
@@ -1,6 +1,6 @@
 import subprocess as sp
 
 sp.check_call('mkdir dist', shell=True)
-tags = sp.check_output('git tag', shell=True).decode('utf-8').split('\n')
+tags = sp.check_output('git tag', shell=True).decode('utf-8').strip().split('\n')
 for tag in tags:
     sp.check_call(f'git checkout tags/{tag} && cargo make build-web && cp -r wasm dist/{tag}', shell=True)
